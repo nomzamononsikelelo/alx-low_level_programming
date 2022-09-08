52b76c14d39c78df980b8a1b1e14986f5e9a813c
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-08 11:36:41 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #include <stdio.h>
 
 /**
@@ -10,7 +8,6 @@
 * Return: 0
 */
 int main(void)
-{
-        puts("\"programming is like building a multilingual puzzle");
-        return(0);
+{    puts("\"programming is like building a multilingual puzzle");
+     return(0);
 }
