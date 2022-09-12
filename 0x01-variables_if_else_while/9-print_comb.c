e25e3fab3367668dfe4ef6dd5ef04f53f756eaab
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-12 16:42:17 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,28 +2,25 @@
 
 /**
  * main - Prints a series of numbers with commas
- *
- * Return: Always(Success)
+ * 
+ * Return: Always(Success);
  */
 int main(void)
 {
 	int c;
 
 	for (c = '0'; c <= '9'; c++)
-
 	{
 		putchar(c);
 
 		if (c != '9')
-
 		{
 			putchar(',');
 			putchar('');
-
 		}
 	}
 
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
