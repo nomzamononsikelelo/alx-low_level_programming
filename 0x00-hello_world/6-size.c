0f6eb4d17fb9c9a6648bf8915b262718740f1a66
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-12 04:51:42 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
         long long lli;
         float f;
 
-        printf("Size of a char: %ld byte(s)\n", sizeof(c));
-        printf("Size of a int: %ld byte(s)\n", sizeof(i));
-        printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-        printf("Size of a float: %ld byte(s)\n", sizeof(f));
+        printf("Size of a char: %1d byte(s)\n", sizeof(c));
+        printf("Size of a int: %1d byte(s)\n", sizeof(i));
+        printf("Size of a long int: %1d byte(s)\n", sizeof(li));
+        printf("Size of a long long int: %1d byte(s)\n", sizeof(lli));
+        printf("Size of a float: %1d byte(s)\n", sizeof(f));
         return (0);
