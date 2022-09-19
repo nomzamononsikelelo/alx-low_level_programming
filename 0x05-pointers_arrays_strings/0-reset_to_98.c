54bcd06c14e0eccf3c3f78fd86b4f223921b623a
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-19 08:35:40 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 -> a function to update the reference of a pointer
- * @n: pointer to n to be updated
+ * reset_to_98 - resets the value of given integer to 98
+ * @n: parameter
+ * Return : nothing
  */
 void reset_to_98(int *n)
 {
