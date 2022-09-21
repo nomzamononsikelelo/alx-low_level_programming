5266c21fd4c1f8365831488fe92ea3bdae0df843
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-21 13:13:06 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,19 +2,22 @@
 
 /**
 * reverse_array -> reversing an array
-* @a: array of integers to be reversed
-* @n: The number of elements in the array
+* @a: array a
+* @n: an element of an array
 */
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int *p, i, aux, k;
 
-	for (index = n - 1; index >= / 2; index--)
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
 	{
-
-		tmp = a[n - 1 - index];
-	        a[n - 1 - index] = a[ondex];
-		a[index] = tmp;
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
 
