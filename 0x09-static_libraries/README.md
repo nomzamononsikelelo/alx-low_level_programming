23e0a466efd249b302f38d20acf9233de56472d9
nomzamononsikelelo <nomzamononsikelelo@github.com>
2022-09-30 20:34:43 -0700

    g

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+# this all about static libraries
+
---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.oo
